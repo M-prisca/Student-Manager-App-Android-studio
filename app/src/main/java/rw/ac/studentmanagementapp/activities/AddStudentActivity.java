3680b2e4c39a090bab35d0c2b6aedeabfe2e8cad
Prisca <priscamasereli@gmail.com>
2025-10-20 15:30:18 +0200

    feat: Add the add student activity

--- app/src/main/java/rw/ac/studentmanagementapp/activities/AddStudentActivity.java
+++ app/src/main/java/rw/ac/studentmanagementapp/activities/AddStudentActivity.java
@@ -2,12 +2,44 @@ package rw.ac.studentmanagementapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import rw.ac.studentmanagementapp.R;
+import rw.ac.studentmanagementapp.database.DatabaseHelper;
 
 public class AddStudentActivity extends AppCompatActivity {
+
+    EditText etName, etAge;
+    Button btnSave;
+    DatabaseHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        btnSave = findViewById(R.id.btnSave);
+        db = new DatabaseHelper(this);
+
+        btnSave.setOnClickListener(v -> {
+            String name = etName.getText().toString();
+            String ageStr = etAge.getText().toString();
+            if(name.isEmpty() || ageStr.isEmpty()) {
+                Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            int age = Integer.parseInt(ageStr);
+            boolean inserted = db.addStudent(name, age);
+            if(inserted) {
+                Toast.makeText(this, "Student added", Toast.LENGTH_SHORT).show();
+                etName.setText("");
+                etAge.setText("");
+            } else {
+                Toast.makeText(this, "Error adding student", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
