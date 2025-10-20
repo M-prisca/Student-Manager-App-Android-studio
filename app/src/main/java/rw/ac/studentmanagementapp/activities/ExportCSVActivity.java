91f4f3ec4af88959319e7c40f128635a7a6ba889
Prisca <priscamasereli@gmail.com>
2025-10-20 15:37:37 +0200

    feat: Add the add export in cvs file activity

--- app/src/main/java/rw/ac/studentmanagementapp/activities/ExportCSVActivity.java
+++ app/src/main/java/rw/ac/studentmanagementapp/activities/ExportCSVActivity.java
@@ -1,13 +1,44 @@
 package rw.ac.studentmanagementapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import rw.ac.studentmanagementapp.R;
+import rw.ac.studentmanagementapp.database.DatabaseHelper;
 
 public class ExportCSVActivity extends AppCompatActivity {
+
+    Button btnExport;
+    DatabaseHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export_csv);
+
+        btnExport = findViewById(R.id.btnExportCSV);
+        db = new DatabaseHelper(this);
+
+        btnExport.setOnClickListener(v -> {
+            try {
+                File file = new File(getExternalFilesDir(null), "students.csv");
+                FileWriter fw = new FileWriter(file);
+                Cursor cursor = db.getAllStudents();
+                while(cursor.moveToNext()) {
+                    fw.append(cursor.getInt(0) + "," + cursor.getString(1) + "," + cursor.getInt(2) + "\n");
+                }
+                fw.flush();
+                fw.close();
+                Toast.makeText(this, "Exported to " + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error exporting CSV", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
